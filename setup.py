a74677eb295f654f669c1f5a9dc2915af2efb82a
anythingtechpro <anythingtechpro@gmail.com>
2018-07-27 14:10:02 -0400

    setup: fixed typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 
 long_description = """
-A replacement for Python's threading module used in attempt to efficient achieve single-core threading parallelism.
+A replacement for Python's threading module used in attempt to efficiently achieve single-core threading parallelism.
 """
 
 setup(name='parallelism',
